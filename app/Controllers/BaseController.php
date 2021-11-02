721155ca0f97a04234a841896854360e8a01bdad
Milan Felix Šulc <rkfelix@gmail.com>
2021-11-02 17:10:06 +0100

    working logging

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -2,46 +2,45 @@
 
 namespace App\Controllers;
 
-use Apitte\Core\Annotation\Controller\GroupId;
-use Apitte\Core\Annotation\Controller\GroupPath;
-use Apitte\Core\Http\ApiRequest;
+use Apitte\Core\Annotation\Controller\Id;
+use Apitte\Core\Annotation\Controller\Path;
 use Apitte\Core\Http\ApiResponse;
 use Apitte\Core\UI\Controller\IController;
 use App\Controllers\Entity\UserTask;
+use Models\Billing;
 use Models\Campaigns;
 use Models\CampaignsNewShare;
 use Models\Company;
 use Models\Devices;
 use Models\Filters;
+use Models\Groups;
 use Models\Locations;
+use Models\Media;
 use Models\Reports;
 use Models\Sales;
 use Models\Users;
-use Models\Media;
 use Models\Video;
-use Models\Groups;
 use Nette\Http\Session;
 use Nette\Utils\Json;
-use Models\Billing;
 
 /**
- * @GroupPath("/api")
- * @GroupId("api")
+ * @Path("/api")
+ * @Id("api")
  */
 abstract class BaseController implements IController
 {
 
-  /**
-   * @inject
-   * @var Session
-   */
-  public $session;
+	/**
+	 * @inject
+	 * @var Session
+	 */
+	public $session;
 
 
-  public function sendResponse(ApiResponse $response, array $data, int $code = 200)
-  {
-    return $response->writeBody(Json::encode($data))
-      ->withHeader("Content-Type", "aplication/json")
-      ->withStatus($code);
-  }
+	public function sendResponse(ApiResponse $response, array $data, int $code = 200)
+	{
+		return $response->writeBody(Json::encode($data))
+			->withHeader("Content-Type", "aplication/json")
+			->withStatus($code);
+	}
 }
